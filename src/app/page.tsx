b494f240963d03799e9dcae89eaf81fd916f9754
Chirag Ajmera <chiragajmera57@gmail.com>
2024-03-07 20:41:12 +0530

    added wallet config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,144 @@
 import Image from "next/image";
-
+import Logo from "../app/asset/logo.png";
+import introLogo from "../app/asset/introductionPage.png";
+import card1 from "../app/asset/card1.png";
+import card2 from "../app/asset/card2.png";
+import card3 from "../app/asset/card3.png";
+import card4 from "../app/asset/card4.png";
+import faq from '../app/asset/faq.png';
+import ConnectButton from "./config/WalletButton";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="body">
+      <div className="hero bg-cover bg-center  bg-no-repeat flex flex-col flex-wrap">
+        <div className="nav flex flex-row justify-evenly align-middle items-center pt-3 flex-wrap">
+          <div className="flex flex-row flex-wrap">
+            <Image className="" alt="dfvd" src={Logo} width={80} />
+            <p className="mt-5 font-ZCOOL font-normal text-lg text-white">
+              Saviour
+            </p>
+          </div>
+          <div className="font-ZCOOL flex-wrap flex flex-row w-1/3 justify-between items-center bg-gray-800 text-white p-2 px-3 rounded-2xl">
+            <p>Home</p>
+            <p>IDO</p>
+            <p>Tokenomics</p>
+            <p>Roadmap</p>
+            <p>Coming Soon</p>
+          </div>
+          <div>
+            
+            <button className="outline-none border-nonerounded-full text-white text-xs font-semibold">
+            <ConnectButton />
+            </button>
+          </div>
+        </div>
+        <div className="title mt-48 font-normal w-1/2  self-center">
+          <p className=" text-5xl text-center text-white font-Lobster">
+            Where Blockchain Heroes Thrive, Rescuing Dreams, Elevating Fortunes.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap p-2 self-center align-middle justify-center  bg-black text-white ">
+        <div className="w-1/2 self-center align-middle justify-center">
+          <p className="text-4xl text-red-500 font-semibold font-Shojumaru">
+            Introduction
+          </p>
+          <div className="text-lg w-1/2 mt-3">
+            We`ve all been in the mud once, and now we`ve decided to fight it
+            out. Pay tribute to those pioneers of WEB3 and the warriors who
+            dedicated their love to the blockchain. Save the lucky ones alive,
+            join us to save the future! Our mission is to empower everyone to
+            share wealth and succeed.{" "}
+            <span className="text-green-50">read more...</span>
+          </div>
+          <button className="outline-none border-none p-3 mt-6 bg-red-500 rounded-full text-white text-xs font-semibold">
+            <p className="mx-3">Documtents</p>
+          </button>
+        </div>
+        <div>
+          <Image src={introLogo} alt="" />
+        </div>
+      </div>
+      <div className="roadmap  bg-no-repeat flex-wrap  bg-cover flex flex-col  ">
+        <div>
+          <p className="text-4xl text-red-500 font-semibold font-Shojumaru text-center pt-9">
+            Road Map
+          </p>
+        </div>
+        <div className="flex gap-40 justify-center mt-32 flex-wrap">
+          <div className="card1 text-white p-5 flex flex-wrap ">
+            <div>
+              <div className="font-bold">Phase 1</div>
+              <div className="text-gray-500">
+                Technical Readiness and Smart Contract Audit
+              </div>
+            </div>
+            <div className="mt-auto"></div>
+          </div>
+
+          <div className="card2 text-white p-5 flex flex-wrap">
+            {" "}
+            <div>
+              <div className="font-bold">Phase 2</div>
+              <div className="text-gray-500">
+                Technical Readiness and Smart Contract Audit
+              </div>
+            </div>
+          </div>
+          <div className="card3 text-white p-5 flex flex-wrap">
+            {" "}
+            <div>
+              <div className="font-bold">Phase 3</div>
+              <div className="text-gray-500">
+                Technical Readiness and Smart Contract Audit
+              </div>
+            </div>
+          </div>
+          <div className="card4 text-white p-5 flex flex-wrap">
+            {" "}
+            <div>
+              <div className="font-bold">Phase 4</div>
+              <div className="text-gray-500">
+                Technical Readiness and Smart Contract Audit
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+      <div className="faq bg-black flex justify-around flex-wrap">
+
+        <div className="mr-8">
+          <Image src={faq} alt="" />
+        </div>
+
+        <div className="text-white pr-32 mt-20 w-1/2 flex flex-wrap flex-col " style={{alignItems:'center'}}>
+            <div className="text-4xl text-red-500 font-semibold font-Shojumaru text-center ">FAQ</div>
+            <div className="flex flex-wrap border-2 p-2  border-red-500 rounded-lg  mb-3  text-center mt-7">
+              <div className="mx-6">1</div>
+              <p className="mx-8">Why choose `Savior`?</p>
+            </div>
+            <div className="flex flex-wrap border-2 p-2  border-red-500 rounded-lg  mb-3  text-center">
+              <div className="mx-6">2</div>
+              <p className="mx-8">Why is the IDO duration so long?</p>
+            </div>
+            <div className="flex flex-wrap border-2 p-2  border-red-500 rounded-lg  mb-3  text-center">
+              <div className="mx-6">3</div>
+              <p className="mx-8">When will trading go live?</p>
+            </div>
+            <div className="flex flex-wrap border-2 p-2  border-red-500 rounded-lg    text-center">
+              <div className="mx-6">4</div>
+              <p className="mx-8">When can we claim the tokens?</p>
+            </div>
+        </div>
+            <div>
+
+            </div>
       </div>
+      <div className="bg-cover fotor w-full bg-black">
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
-    </main>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily:{
+        ZCOOL:['ZCOOL KuaiLe'],
+        Shojumaru:['ZCOOL KuaiLe','sans-serif'],
+        Lobster:['Lobster','sans-serif'],
+      }
+     
     },
   },
   plugins: [],
